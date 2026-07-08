6b15fd03120b71abafc3cd7a98ad14174197b855
Tanpaisen <tinh7demtb@gmail.com>
2026-07-08 22:22:25 +0700

    Lọc theo trạng thái

--- api/v1/controllers/task.controller.ts
+++ api/v1/controllers/task.controller.ts
@@ -2,13 +2,26 @@ import { Request, Response } from 'express';
 import Task from '../../../models/tasks.model';
 
 export const index = async (req: Request, res: Response): Promise<void> => {
-    const tasks = await Task.find({
+    interface Find {
+        deleted: boolean,
+        status?: string
+    }
+
+    const find: Find = {
         deleted: false
-    })
+    }
+
+    //Lọc theo trạng thái
+    if (req.query.status) {
+        find["status"] = req.query.status.toString()
+    }
+    //End lọc theo trạng thái
+
+    const tasks = await Task.find(find)
     res.json(tasks)
 };
 
-export const detail = async (req: Request, res: Response): Promise<void>  => {
+export const detail = async (req: Request, res: Response): Promise<void> => {
     const id = req.params.id;
     const task = await Task.findOne({
         _id: id,
